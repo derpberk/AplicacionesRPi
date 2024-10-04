fe92e08d29dd3848851e0b64a69a5cf65662f2be
Samuel Yanes <derpberk@gmail.com>
2024-10-04 10:42:29 +0200

    Fixed camera_1

--- practica3/camera_1.py
+++ practica3/camera_1.py
@@ -1,31 +1,31 @@
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 import gpiozero
 from time import sleep
-
+import libcamera
 
 # Creamos un objeto tipo Button 
 button = gpiozero.Button("GPIO1", pull_up=False)
 
 # Instanciamos la clase PiCamera2
-camera = PiCamera2()
-# Configuramos la resolución y rotación de la cámara
-camera.resolution = (640,480)
-camera.rotation = 180
-# Iniciamos la vista previa de la cámara
+camera = Picamera2()
 
-index = 0
+# Configuramos la resolución, visualizacion y rotación de la cámara
+camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+camera_config['transform'] = libcamera.Transform(vflip=True) 
+camera.configure(camera_config)
 
-while True:
+for index in range(5):
     
     # Usamos el método wait_for_press() 
     # para esperar a que el botón sea presionado
     button.wait_for_press() 
     # Iniciamos la vista previa de la cámara
-    camera.start_preview(fullscreen=False, window=(30,30,320,240)) 
+    camera.start_preview(Preview.QTGL)
     sleep(5) # Esperamos 5 segundos
     camera.stop_preview() # Detenemos la vista previa
-    camera.capture(f'/home/pi/captura_{index}.jpg') # Capturamos!
+    camera.capture_file(f'/home/pi/captura_{index}.jpg') # Capturamos!
     sleep(1) # Esperamos 1 segundo para evitar capturas consecutivas
+    
     
     
 

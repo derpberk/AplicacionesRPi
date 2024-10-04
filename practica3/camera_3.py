12d106b908c3283f8fe94f603993a797246359c9
Samuel Yanes <derpberk@gmail.com>
2024-10-04 16:09:02 +0100

    Fixed camera_3.py

--- practica3/camera_3.py
+++ practica3/camera_3.py
@@ -5,8 +5,11 @@ from picamera2 import Picamera2
 cv2.startWindowThread()
 # Creamos el objeto de la RaspiCam
 picam2 = Picamera2()
-# Configuramos la resolución, framerate y formato de la cámara
-config = {"format": 'BGR888', "size": (640, 480), "framerate": 24}
+# Modificamos el framrate (fps)
+picam2.video_configuration.controls.FrameRate = 25.0
+
+# Configuramos la resolución y formato de la cámara
+config = {"format": 'BGR888', "size": (640, 480)}
 picam2.configure(picam2.create_preview_configuration(main=config))
 # Iniciamos la vista previa de la cámara
 picam2.start()
@@ -16,7 +19,6 @@ while True:
     im_array = picam2.capture_array()
     # Convertimos la imagen a escala de grises
     grey = cv2.cvtColor(im_array, cv2.COLOR_BGR2GRAY)
-
     # Mostramos la imagen en una ventana de OpenCV llamada "Camera"
     cv2.imshow("Camera output", grey)
     # Esperamos 1ms para que la ventana de OpenCV no se cierre
